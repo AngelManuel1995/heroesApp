c9892c1f37824c15e355dc83abded4a30084336a
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-02-28 19:32:53 -0500

    Hero componente creates new hero

--- src/app/componentes/heroes/hero.component.ts
+++ src/app/componentes/heroes/hero.component.ts
@@ -1,16 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router'
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
     selector:'app-hero',
     templateUrl:'./hero.component.html'
 })
 
-export class HeroComponent{
+export class HeroComponent implements OnInit {
     
-    test:string = "it works";
+    hero:Hero = {
+        name:'',
+        bio:'',
+        house:'Marvel'
+    }
 
-    constructor(){
 
+		constructor( private _heroesService:HeroesService, 
+								 private _router:Router){
     
     }
+
+    ngOnInit() {
+        
+    }
+
+    saveHero(){
+			 this._heroesService.newHero(this.hero)
+			 	.subscribe( data => {
+					this._router.navigate(['/hero', data.name]);
+				}, ( e ) => {
+					console.error(e);
+				});
+			}
+
+    /**
+     *JSON.stringify(object): Funcion que permite convertir un objeto a un string
+     *JSON.parse(objectString): Función que nos permite convertir un string a un objeto.
+     */ 
+
 }
